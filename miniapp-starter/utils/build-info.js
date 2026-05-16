f3c2f2801133d1a57104e0ecb6506aea1752d595
zhangting <zhangting@taou.com>
2026-05-16 12:32:22 +0800

    build: bump 1.0.0.26051602 (规划模式 + 我的页面加版本号)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: 'dev',
-  commitId: 'unknown',
-  builtAt: 0
+  version: '1.0.0.26051602',
+  commitId: 'eb7c41e',
+  builtAt: 1778905935810
 }
