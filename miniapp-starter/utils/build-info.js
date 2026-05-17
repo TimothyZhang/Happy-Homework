581aff2b49b6de42dcaa6ba8b058ccee10a87d9c
zhangting <zhangting@taou.com>
2026-05-17 18:36:47 +0800

    build: 1.0.0.26051728

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26051727',
-  commitId: 'b528011',
-  builtAt: 1779013921084
+  version: '1.0.0.26051728',
+  commitId: '0db58f0',
+  builtAt: 1779014207516
 }
