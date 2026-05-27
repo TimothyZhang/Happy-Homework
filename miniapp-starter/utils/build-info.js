243c999717216b7516b130b3d98c90bc603b1bcd
zhangting <zhangting@taou.com>
2026-05-27 13:52:52 +0800

    build: 1.0.0.26052701 (home: perfect toast 不被 overdue 挡)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26052601',
-  commitId: 'b746dc7',
-  builtAt: 1779808299468
+  version: '1.0.0.26052701',
+  commitId: 'a436007',
+  builtAt: 1779861172731
 }
