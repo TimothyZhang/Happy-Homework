b3aa185ebb95605c11564d2ed0e91ecd0d60c932
zhangting <zhangting@taou.com>
2026-06-05 11:58:07 +0800

    build: 1.0.0.26060501 (宠物点击改按心情反应,移除开发用动画循环)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26052911',
-  commitId: '195a4a6',
-  builtAt: 1780127271566
+  version: '1.0.0.26060501',
+  commitId: 'f6ea17d',
+  builtAt: 1780631887445
 }
