52cba07722abab4a00363dcff588a3c33671b65c
zhangting <zhangting@taou.com>
2026-06-05 22:17:20 +0800

    build: 1.0.0.26060519 (答对飘奖励动画+结算双奖励;新词不剧透;修我tab重叠)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060518',
-  commitId: '2597a76',
-  builtAt: 1780668033606
+  version: '1.0.0.26060519',
+  commitId: 'db6b453',
+  builtAt: 1780669040033
 }
