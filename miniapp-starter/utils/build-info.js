bf1af1bfc6922ce8ecdfdb415b93b8577e591452
zhangting <zhangting@taou.com>
2026-06-05 14:43:24 +0800

    build: 1.0.0.26060502 (宠物 2.5D 房间场景 + 四方向行走)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060501',
-  commitId: 'f6ea17d',
-  builtAt: 1780631887445
+  version: '1.0.0.26060502',
+  commitId: '6c856d5',
+  builtAt: 1780641804666
 }
