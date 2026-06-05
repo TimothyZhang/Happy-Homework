289ffe17ba0dc757b4bc2e4acb49aec94b9fc7b9
zhangting <zhangting@taou.com>
2026-06-06 00:15:56 +0800

    build: 1.0.0.26060522 (修听写无声:播放重建AudioContext)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060521',
-  commitId: 'd963e5d',
-  builtAt: 1780673885415
+  version: '1.0.0.26060522',
+  commitId: '315dd0f',
+  builtAt: 1780676156191
 }
