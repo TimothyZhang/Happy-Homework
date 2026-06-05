a341dc8b7c325f4982c4f81de89e7dcbfa1aa26d
zhangting <zhangting@taou.com>
2026-06-05 17:01:09 +0800

    build: 1.0.0.26060508 (跟宠物背单词 + 自制键盘)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060507',
-  commitId: 'a75ab2b',
-  builtAt: 1780648750521
+  version: '1.0.0.26060508',
+  commitId: 'aba9638',
+  builtAt: 1780650069026
 }
