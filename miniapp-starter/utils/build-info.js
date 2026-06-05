b8dc3700ece30062998b3ffd02bff0c092e88bf3
zhangting <zhangting@taou.com>
2026-06-05 18:16:21 +0800

    build: 1.0.0.26060513 (单词本拍照OCR导入)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060512',
-  commitId: '8d54306',
-  builtAt: 1780654013991
+  version: '1.0.0.26060513',
+  commitId: 'e1e0470',
+  builtAt: 1780654580952
 }
