dcd64f75e3ff56e61bfad5de5ece3e478fd63a05
zhangting <zhangting@taou.com>
2026-06-05 16:18:36 +0800

    build: 1.0.0.26060505 (说话气泡挂到宠物头顶)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060504',
-  commitId: 'd9ab095',
-  builtAt: 1780646054705
+  version: '1.0.0.26060505',
+  commitId: '5396953',
+  builtAt: 1780647516705
 }
