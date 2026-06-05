4a7a1363d15bb4382c567471f2a3762556871963
zhangting <zhangting@taou.com>
2026-06-05 21:48:16 +0800

    build: 1.0.0.26060516 (听写 Azure OpenAI TTS 云函数版)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060515',
-  commitId: 'a5ab1d5',
-  builtAt: 1780661783152
+  version: '1.0.0.26060516',
+  commitId: 'ec71b42',
+  builtAt: 1780667296897
 }
