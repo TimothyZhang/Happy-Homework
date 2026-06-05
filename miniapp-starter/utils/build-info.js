05e20cdd15a22fcc43ebdb89cf532f245314e396
zhangting <zhangting@taou.com>
2026-06-05 17:56:56 +0800

    build: 1.0.0.26060511 (背单词SRS + 宠物知识属性 + 每日3次)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060510',
-  commitId: 'e14fb24',
-  builtAt: 1780651520619
+  version: '1.0.0.26060511',
+  commitId: '44d125a',
+  builtAt: 1780653416016
 }
