158149ceb7868dd289c36744c6badd5a0f43c153
zhangting <zhangting@taou.com>
2026-06-05 16:39:10 +0800

    build: 1.0.0.26060507 (修说话气泡变窄变高)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060506',
-  commitId: '98d0fa7',
-  builtAt: 1780648568721
+  version: '1.0.0.26060507',
+  commitId: 'a75ab2b',
+  builtAt: 1780648750521
 }
