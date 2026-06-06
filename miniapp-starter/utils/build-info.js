bc32841544ea18ef7dbcbadae9a374f3d803cbe8
zhangting <zhangting@taou.com>
2026-06-06 15:04:27 +0800

    build: 1.0.0.26060603 (宠物更可爱:大眼睛+待机小跳+摸摸它飘爱心)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060602',
-  commitId: '0da3595',
-  builtAt: 1780726430951
+  version: '1.0.0.26060603',
+  commitId: 'a71831e',
+  builtAt: 1780729467022
 }
