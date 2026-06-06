f73d66d2721c94f0d4cbe33af183b0453d683e61
zhangting <zhangting@taou.com>
2026-06-06 18:17:38 +0800

    build: 1.0.0.26060609 (城堡窗外去掉圆盘)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060608',
-  commitId: '6b68470',
-  builtAt: 1780741058713
+  version: '1.0.0.26060609',
+  commitId: 'd6c282b',
+  builtAt: 1780741393094
 }
