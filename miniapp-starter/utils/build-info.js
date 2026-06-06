ad40aa208fd6c9c42d9a8559b2badee894be8d30
zhangting <zhangting@taou.com>
2026-06-06 19:14:19 +0800

    build: 1.0.0.26060612 (管理项并入商店弹窗+相机跟随宠物居中)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060611',
-  commitId: 'f53e58a',
-  builtAt: 1780742484279
+  version: '1.0.0.26060612',
+  commitId: '74947b1',
+  builtAt: 1780744459185
 }
