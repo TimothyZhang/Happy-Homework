52c4399eb98c1c7e696c47ff4e5550675d066a5a
zhangting <zhangting@taou.com>
2026-06-06 09:07:44 +0800

    build: 1.0.0.26060527 (听写音色换男声WeJack)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060526',
-  commitId: '672ccfb',
-  builtAt: 1780707435001
+  version: '1.0.0.26060527',
+  commitId: '3975a6d',
+  builtAt: 1780708064020
 }
