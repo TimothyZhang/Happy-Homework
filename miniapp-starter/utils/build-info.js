83c0c2bd5c6a0ec0ba97ae833f8626a9c0f8adbc
zhangting <zhangting@taou.com>
2026-06-07 05:08:34 +0800

    build: 1.0.0.26060626 (房间家具PNG+交互, XP测试对齐)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060625',
-  commitId: 'c125c28',
-  builtAt: 1780764717797
+  version: '1.0.0.26060626',
+  commitId: 'd485088',
+  builtAt: 1780780114779
 }
