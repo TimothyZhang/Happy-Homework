ce08fc998ddb8b11f2484019f23cf80885ed55ac
zhangting <zhangting@taou.com>
2026-06-07 00:51:57 +0800

    build: 1.0.0.26060625 (宠物房间家具)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060624',
-  commitId: 'db6ffed',
-  builtAt: 1780764472982
+  version: '1.0.0.26060625',
+  commitId: 'c125c28',
+  builtAt: 1780764717797
 }
