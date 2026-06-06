e2e69d05049997328ad9aa870581751a2933abff
zhangting <zhangting@taou.com>
2026-06-06 17:08:23 +0800

    build: 1.0.0.26060606 (果冻rig铺满9物种 + 学习小书桌)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060605',
-  commitId: 'd8a370a',
-  builtAt: 1780735178982
+  version: '1.0.0.26060606',
+  commitId: 'e3408d7',
+  builtAt: 1780736903574
 }
