8efbdd6bf71048f4cb3b6872638cfe26f8537c33
zhangting <zhangting@taou.com>
2026-06-06 18:05:58 +0800

    build: 1.0.0.26060607 (房间扩大2倍 + 可选城堡背景)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060606',
-  commitId: 'e3408d7',
-  builtAt: 1780736903574
+  version: '1.0.0.26060607',
+  commitId: '5fe8bfe',
+  builtAt: 1780740358434
 }
