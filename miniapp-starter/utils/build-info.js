a7dc8a16dca67c24ffce6aeb9112940fa5463ca0
zhangting <zhangting@taou.com>
2026-06-06 20:01:05 +0800

    build: 1.0.0.26060613 (相机平移改transform+ease-in-out慢加速减速)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060612',
-  commitId: '74947b1',
-  builtAt: 1780744459185
+  version: '1.0.0.26060613',
+  commitId: '93e58df',
+  builtAt: 1780747265204
 }
