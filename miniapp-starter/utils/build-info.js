4e1b285196c5b1b9bd6d46dee0bb3311e0504d2f
zhangting <zhangting@taou.com>
2026-06-06 13:37:53 +0800

    build: 1.0.0.26060537 (公开单词库+单词编辑+背单词改名单词挑战+重听/间距)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060536',
-  commitId: 'c912398',
-  builtAt: 1780722697852
+  version: '1.0.0.26060537',
+  commitId: '6bbcaf5',
+  builtAt: 1780724273322
 }
