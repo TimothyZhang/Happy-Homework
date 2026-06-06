aaf6813ff7cbe5d2c7eda3c7c7c5e139675b79b5
zhangting <zhangting@taou.com>
2026-06-06 18:34:28 +0800

    build: 1.0.0.26060610 (经验隐藏+商店挪金币下按钮)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060609',
-  commitId: 'd6c282b',
-  builtAt: 1780741393094
+  version: '1.0.0.26060610',
+  commitId: '4677802',
+  builtAt: 1780742068754
 }
