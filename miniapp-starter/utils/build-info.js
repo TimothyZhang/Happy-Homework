9a722511ad644f455241b22c8468189f10faa8fc
zhangting <zhangting@taou.com>
2026-06-07 09:18:13 +0800

    build: 1.0.0.26060631 (免费道具命名+冷却倒计时)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060630',
-  commitId: '5782a72',
-  builtAt: 1780794796952
+  version: '1.0.0.26060631',
+  commitId: '3e92084',
+  builtAt: 1780795093383
 }
