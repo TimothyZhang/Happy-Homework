731467de118372664cd3dbd6da4a772849d3a23a
zhangting <zhangting@taou.com>
2026-06-07 08:11:13 +0800

    build: 1.0.0.26060627 (家具回属性+冷却)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060626',
-  commitId: 'd485088',
-  builtAt: 1780780114779
+  version: '1.0.0.26060627',
+  commitId: '5c11bec',
+  builtAt: 1780791073904
 }
