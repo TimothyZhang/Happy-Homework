bf8b5ea0f3d5b46434bfef35cbf4b2828e3d20fc
zhangting <zhangting@taou.com>
2026-06-08 09:12:49 +0800

    build: 1.0.0.26060801 (宠物气泡换行)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060636',
-  commitId: '832440b',
-  builtAt: 1780807331513
+  version: '1.0.0.26060801',
+  commitId: '2405a46',
+  builtAt: 1780881169343
 }
