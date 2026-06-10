63d4cf85147e44ead6dba4397b5b4fea9a31c8bc
zhangting <zhangting@taou.com>
2026-06-10 20:41:18 +0800

    build: 1.0.0.26060804 (书桌移中间+家具好点)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060803',
-  commitId: 'b6f2f79',
-  builtAt: 1781094714982
+  version: '1.0.0.26060804',
+  commitId: 'f7b643c',
+  builtAt: 1781095278844
 }
