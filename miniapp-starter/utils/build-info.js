d22ce8d630b5526cbb4870e3ad4b8fb8f2278bed
zhangting <zhangting@taou.com>
2026-06-12 23:35:33 +0800

    build: 1.0.0.26061201 (家具下移+iPad菜单可点)

--- miniapp-starter/utils/build-info.js
+++ miniapp-starter/utils/build-info.js
@@ -1,6 +1,6 @@
 // 自动生成 —— 由 scripts/write-build-info.js 写入,不要手改。
 module.exports = {
-  version: '1.0.0.26060804',
-  commitId: 'f7b643c',
-  builtAt: 1781095278844
+  version: '1.0.0.26061201',
+  commitId: '7e888bf',
+  builtAt: 1781278533271
 }
